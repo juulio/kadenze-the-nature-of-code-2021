78aca6b4d8a1ef7208e9b2d2a39df94636ef8e76
Julio Del Valle <juulio@gmail.com>
2021-10-26 22:19:10 -0600

    01:Random Walker -> Adding Pulse with noise

--- 01_randomwalker/01_randomwalker.js
+++ 01_randomwalker/01_randomwalker.js
@@ -1,20 +1,46 @@
 class Walker {
-    constructor(x, y) {
-        this.pos = createVector(width / 2, 0);
+    constructor(x, y, t) {
+        this.pos = createVector(x, y);
         this.vel = createVector(0, 0);
         this.acc = createVector(0, 0.01);
+        this.size = createVector(20, 20);
+        this.t = 0;
     }
     
     update() {
         this.vel.add(this.acc);
         this.pos.add(this.vel);
-        // this.vel.limit(4);
+        this.t = this.t + 0.04;
     }
 
     display() {
         stroke(255);
         strokeWeight(2);
         fill(255, 0, 0);
-        ellipse(this.pos.x, this.pos.y, 30, 30);
+        ellipse(this.pos.x, this.pos.y, this.size.x, this.size.y);
+    }
+
+    checkEdges() {
+        if (this.pos.x > VIEWPORT_WIDTH) {
+            this.pos.x = 0;
+        } 
+        else if (this.pos.x < 0) {
+            this.pos.x = width;
+        }
+    
+        if (this.pos.y > VIEWPORT_HEIGHT) {
+            this.pos.y = 0;
+        } 
+        else if (this.pos.y < 0) {
+            this.pos.y = height;
+        }
+    }
+    
+    pulse() {
+        let sizeDelta = noise(this.t);
+        sizeDelta = map(sizeDelta, 0, 1, 10, 60);
+        
+        this.size.x = sizeDelta;
+        this.size.y = sizeDelta;
     }
 }
--- 01_randomwalker/sketch.js
+++ 01_randomwalker/sketch.js
@@ -1,14 +1,19 @@
 import Walker from './01_randomwalker';
 
+const VIEWPORT_WIDTH = 500;
+const VIEWPORT_HEIGHT = 500;
+
 let walker;
 
 function setup() {
-    createCanvas(600, 500);
-    walker = new Walker(200, 200);
+    createCanvas(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+    walker = new Walker(200, 100);
 }
 
 function draw() {
     background(220);
     walker.update();
+    walker.checkEdges();
+    walker.pulse();
     walker.display();
 }
